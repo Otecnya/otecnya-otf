f961a898aea30388fe3f2766a40df3c671ebadf6
Jonathan Delgado hi@jon.soy <hi@jon.soy>
2016-06-05 01:44:24 -0400

    - babel support  - browserify support

--- Gulpfile.babel.js
+++ Gulpfile.babel.js
@@ -1,24 +1,15 @@
 import Gulp from 'gulp'
 import path from 'path'
-import Browserify from 'browserify'
-import Babelify from 'babelify'
-import stream from 'vinyl-source-stream'
+import babel from 'gulp-babel'
 
 let Task = Gulp.task.bind( Gulp )
 let Src = Gulp.src.bind( Gulp )
 let Dest = Gulp.dest.bind( Gulp )
 
-Task( 'build', () => {
-	Browserify( {
-			entries: [ 'src/OTF.js' ],
-			extensions: [ '.js' ],
-		} )
-		.transform( Babelify, {
-			presets: [ 'es2015' ],
-		} )
-		.bundle()
-		.pipe( stream( 'OTF.js' ) )
-		.pipe( Gulp.dest( 'lib' ) )
-} )
+Task( 'build', () => Src( [ 'src/**/*.js' ] )
+	.pipe( babel( {
+		presets: [ "es2015" ]
+	} ) )
+	.pipe( Dest( 'lib' ) ) )
 
 Task( 'default', [ 'build' ] )
